082623d437e41edbf9cba333d7387a79d72af0ef
Parth Karalkar <parthkaralkar@gmail.com>
2025-12-12 10:33:33 +0530

    Added carbs and fat fields to FoodItem model

--- store/models.py
+++ store/models.py
@@ -4,6 +4,8 @@ class FoodItem(models.Model):
     name = models.CharField(max_length=200)
     calories = models.IntegerField()
     protein = models.FloatField()
+    carbs = models.FloatField(default=0.0)
+    fat = models.FloatField(default=0.0)
     
     def __str__(self):
         return self.name
